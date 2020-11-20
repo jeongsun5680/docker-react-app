02feb11df9909fd4e5c836ebb7d1523be6c5f69f
unknown <wjdjeong4067261@gmail.com>
2020-11-20 20:03:42 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          나도 삼성전자 취업하고싶다, 할 수 있다
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
